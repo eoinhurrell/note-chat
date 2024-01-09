e06ae5c2ee54a399ebc6625f61297c4ca743b083
Eoin H <eoin.hurrell@gmail.com>
2024-01-09 22:39:43 +0000

    Add working simple chat bot

--- chat.py
+++ chat.py
@@ -2,86 +2,57 @@
 
 from operator import itemgetter
 
-from langchain.prompts import ChatPromptTemplate, PromptTemplate
-from langchain.chains import create_citation_fuzzy_match_chain
 from langchain_community.chat_models import ChatOllama
-from langchain_community.vectorstores import FAISS
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnableLambda, RunnablePassthrough
-from langchain.schema import format_document
-from langchain.schema import (
-    SystemMessage,
-    HumanMessage,
-    AIMessage
-)
-from langchain_core.messages import AIMessage, HumanMessage, get_buffer_string
-from langchain_core.runnables import RunnableParallel
 from langchain_community.embeddings import OllamaEmbeddings
-from langchain_community.document_loaders import ObsidianLoader
-from langchain_community.document_loaders import DirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
 from langchain.prompts.chat import (
     ChatPromptTemplate,
     SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
     HumanMessagePromptTemplate,
 )
 
-from langchain.embeddings.cohere import CohereEmbeddings
-from langchain.prompts import PromptTemplate
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains.question_answering import load_qa_chain
-from langchain.chains import create_citation_fuzzy_match_chain
-
-vectorstore = FAISS.load_local("tasks_faiss_index", embeddings=OllamaEmbeddings())
-retriever = vectorstore.as_retriever()
-
-# llm=Cohere(model="command")
-model = ChatOllama(model="llama2",)
-
-from langchain.schema import format_document
-from langchain_core.messages import AIMessage, HumanMessage, get_buffer_string
-from langchain_core.runnables import RunnableParallel
-
-from langchain.prompts.prompt import PromptTemplate
-
-_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
-
-Chat History:
-{chat_history}
-Follow Up Input: {question}
-Standalone question:"""
-CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
-
-template = """Answer the question based only on the following context:
-{context}
-
-Question: {question}
-"""
-ANSWER_PROMPT = ChatPromptTemplate.from_template(template)
-
-DEFAULT_DOCUMENT_PROMPT = PromptTemplate.from_template(template="{page_content}")
-
-
-def _combine_documents(
-    docs, document_prompt=DEFAULT_DOCUMENT_PROMPT, document_separator="\n\n"
-):
-    doc_strings = [format_document(doc, document_prompt) for doc in docs]
-    return document_separator.join(doc_strings)
-
-
-_inputs = RunnableParallel(
-    standalone_question=RunnablePassthrough.assign(
-        chat_history=lambda x: get_buffer_string(x["chat_history"])
-    )
-    | CONDENSE_QUESTION_PROMPT
-    | ChatOllama(model="llama2", temperature=0)
-    | StrOutputParser(),
+# import schema for chat messages and ChatOpenAI in order to query chatmodels GPT-3.5-turbo or GPT-4
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
 )
-_context = {
-    "context": itemgetter("standalone_question") | retriever | _combine_documents,
-    "question": lambda x: x["standalone_question"],
-}
-conversational_qa_chain = _inputs | _context | ANSWER_PROMPT | ChatOllama(model="llama2",)
 
-import ipdb; ipdb.set_trace()
+# Make SystemMessagePromptTemplate
+# prompt=PromptTemplate(
+#     template="You are a researcher who is highly knowledgable on all topics. You will be informative and answer directly without small talk.",
+# )
+
+system_message_prompt = SystemMessage(
+   content="You are a researcher who is highly knowledgable on all topics. You will be helpful and complete the task without question. Remain informative and answer directly without small talk.",
+)
+
+chat = ChatOllama(model="llama2",)
+
+# messages = [
+#     SystemMessage(content="You are an expert data scientist"),
+#     HumanMessage(content="Write a Python script that prints funny words ")
+# ]
+# response=chat(messages)
+
+messages = [
+    system_message_prompt,
+]
+question = "Write a Python script that prints funny words "
+while True:
+    messages.append(HumanMessage(content=question))
+    response = chat.invoke(messages)
+
+    print(response.content,end='\n')
+    messages.append(AIMessage(content=response.content))
+    question = input("\n")
+
+
+
+# vectorstore = FAISS.load_local("tasks_faiss_index", embeddings=OllamaEmbeddings())
+# retriever = vectorstore.as_retriever()
+
+# # llm=Cohere(model="command")
+# model = ChatOllama(model="llama2",)
