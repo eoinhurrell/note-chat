98ab37b56a33f2ff9fc7c62e2157359980d150e6
Eoin Hurrell <eoinhurrell@users.noreply.github.com>
2024-01-18 11:07:01 +0000

    Cleanup build script

--- build.py
+++ build.py
@@ -1,24 +1,13 @@
-
 #!/usr/bin/env python3
 
 from operator import itemgetter
 
-from langchain.prompts import ChatPromptTemplate
-from langchain_community.chat_models import ChatOpenAI
-from langchain_community.chat_models import ChatOllama
-from langchain_community.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnableLambda, RunnablePassthrough
-from langchain.schema import format_document
-from langchain_core.messages import AIMessage, HumanMessage, get_buffer_string
-from langchain_core.runnables import RunnableParallel
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.document_loaders import ObsidianLoader
-# from obsidian import ObsidianLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-loader = ObsidianLoader('/mnt/d/Dropbox/Tasks')
+loader = ObsidianLoader("/mnt/d/Dropbox/Tasks")
 docs = loader.load()
 
 # print(docs[0])
