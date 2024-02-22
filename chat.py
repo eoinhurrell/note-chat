3efff10d43f224185e3a1e91b1d6f8753bcf1294
Eoin Hurrell <eoinhurrell@users.noreply.github.com>
2024-02-22 16:08:15 +0000

    Cleanup

--- chat.py
+++ chat.py
@@ -4,42 +4,120 @@ from langchain_community.chat_models import ChatOllama
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.schema import format_document
+from langchain_core.messages import get_buffer_string
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 from langchain_community.vectorstores import FAISS
+from operator import itemgetter
 
-vectorstore = FAISS.load_local("tasks_faiss_index", embeddings=OllamaEmbeddings())
+from langchain.memory import ConversationBufferMemory
+import streamlit as st
+
+
+vectorstore = FAISS.load_local(
+    "tasks_faiss_index", embeddings=OllamaEmbeddings())
 retriever = vectorstore.as_retriever()
-chat = ChatOllama(
-    model="mistral",
+chat = ChatOllama(model="mistral")
+
+memory = ConversationBufferMemory(
+    return_messages=True, output_key="answer", input_key="question"
 )
+# First we add a step to load memory
+# This adds a "memory" key to the input object
+loaded_memory = RunnablePassthrough.assign(
+    chat_history=RunnableLambda(
+        memory.load_memory_variables) | itemgetter("history"),
+)
+DEFAULT_DOCUMENT_PROMPT = PromptTemplate.from_template(
+    template="{page_content}")
 
 
-template = """Use the following search results from my notes to answer the question at the end.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
-Use three sentences maximum and keep the answer as concise as possible.
+def _combine_documents(
+    docs, document_prompt=DEFAULT_DOCUMENT_PROMPT, document_separator="\n\n"
+):
+    doc_strings = [format_document(doc, document_prompt) for doc in docs]
+    return document_separator.join(doc_strings)
 
+
+_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
+
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:"""
+CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
+
+template = """Answer the question based only on the following context:
 {context}
 
 Question: {question}
+"""
+ANSWER_PROMPT = ChatPromptTemplate.from_template(template)
 
-Helpful Answer:"""
-custom_rag_prompt = PromptTemplate.from_template(template)
-
-
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
-
-
-rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | custom_rag_prompt
+# Now we calculate the standalone question
+standalone_question = {
+    "standalone_question": {
+        "question": lambda x: x["question"],
+        "chat_history": lambda x: get_buffer_string(x["chat_history"]),
+    }
+    | CONDENSE_QUESTION_PROMPT
     | chat
-    | StrOutputParser()
-)
-question = "Write a Python script that prints funny words "
-while True:
-    # response = chat.invoke(messages)
-    response = rag_chain.invoke(question)
+    | StrOutputParser(),
+}
 
-    print(response, end="\n")
-    question = input("\n")
+# Now we retrieve the documents
+retrieved_documents = {
+    "docs": itemgetter("standalone_question") | retriever,
+    "question": lambda x: x["standalone_question"],
+}
+# Now we construct the inputs for the final prompt
+final_inputs = {
+    "context": lambda x: _combine_documents(x["docs"]),
+    "question": itemgetter("question"),
+}
+# And finally, we do the part that returns the answers
+answer = {
+    "answer": final_inputs | ANSWER_PROMPT | chat,
+    "docs": itemgetter("docs"),
+}
+# And now we put it all together!
+final_chain = loaded_memory | standalone_question | retrieved_documents | answer
+
+st.title("ChatGPT-like clone")
+# Set a default model
+if "model" not in st.session_state:
+    st.session_state["model"] = final_chain
+
+# Initialize chat history
+if "memory" not in st.session_state:
+    st.session_state["memory"] = memory
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+    response = final_chain.invoke({"question": prompt})
+    memory.save_context({"question": prompt}, {
+                        "answer": response["answer"].content})
+    full_response = f"""{response['answer'].content}"""
+    # for doc in response["answer"]["doc"]:
+    #     full_response += f"\n- {doc['path']}"
+    full_response += "\n"
+    st.session_state.messages.append(
+        {"role": "assistant", "content": full_response})
